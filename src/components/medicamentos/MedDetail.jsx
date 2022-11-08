60d4cd864d232cc2dea1434973e676b6557de222
gerashdo <gerardo.jimenezarguelles@gmail.com>
2022-11-08 12:26:32 -0600

    Mover los detalles del med a su propio componente

--- src/components/medicamentos/MedDetail.jsx
+++ src/components/medicamentos/MedDetail.jsx
@@ -1,8 +1,45 @@
-import React from 'react'
+import { MedSmallCard } from "./MedSmallCard"
+import { useForm } from "../../hooks/useForm"
 
-export const MedDetail = ({ }) => {
+
+export const MedDetail = ({ medicamento }) => {
+    const [ values, handleChange ] = useForm({ descripcion: ''})
+    const { descripcion } = values
+
+    const handleRemoveMed = ( id ) => {
+        console.log( id )
+    }
     
     return (
-        <div>MedDetail</div>
+        <>
+        
+            <div className="med-titlename">
+                <h3>{ medicamento.name } { medicamento.ch }</h3>
+            </div>
+            <div className="med-data">
+                {
+                    medicamento.medicines
+                    && 
+                    (<ul>
+                        {
+                            medicamento.medicines.map( med => (
+                                <MedSmallCard
+                                    key={ med.id } 
+                                    medicamento={ med }
+                                    onCloseInnerMed={ handleRemoveMed }
+                                />
+                            ))
+                        }
+                    </ul>)
+                }
+                <form action="">
+                    <textarea 
+                        name="descripcion"
+                        value={ descripcion }
+                        onChange={ handleChange }
+                    />
+                </form>
+            </div>
+        </>
     )
 }
--- src/pages/MedPage.jsx
+++ src/pages/MedPage.jsx
@@ -1,20 +1,16 @@
-import { useMemo } from "react"
 import { useSelector } from "react-redux"
 import { useParams } from "react-router-dom"
 import { TemplateSystemPage } from "./TemplateSystemPage"
-
-import './med-page.css'
-import { MedSmallCard } from "../components/medicamentos/MedSmallCard"
-import { useForm } from "../hooks/useForm"
 import { useEffect } from "react"
 import { useState } from "react"
+import { MedDetail } from "../components/medicamentos/MedDetail"
+
+import './med-page.css'
 
 
 export const MedPage = () => {
     const { medId } = useParams()
     const { medicamentos } = useSelector( state => state.medicamento  )
-    const [ values, handleChange ] = useForm({ descripcion: ''})
-    const { descripcion } = values
 
     const getMedicamentoById = ( id ) => {
         return medicamentos.find( med => med.id === id )
@@ -30,47 +26,15 @@ export const MedPage = () => {
     }, [medicamentos, medId])
     
 
-    const handleRemoveMed = ( id ) => {
-        console.log( id )
-    }
-
-    // TODO: Mover codigo a MedDetail
-
     return (
         <TemplateSystemPage>
             <main className="med">
-                <section className="section med-detail">
+                <section className="section med-detail-container">
                     {   
                         medicamento
-                        ? (<>
-                            <div className="med-titlename">
-                                <h3>{ medicamento.name } { medicamento.ch }</h3>
-                            </div>
-                            <div className="med-data">
-                                {
-                                    medicamento.medicines
-                                    && 
-                                    (<ul>
-                                        {
-                                            medicamento.medicines.map( med => (
-                                                <MedSmallCard
-                                                    key={ med.id } 
-                                                    medicamento={ med }
-                                                    onCloseInnerMed={ handleRemoveMed }
-                                                />
-                                            ))
-                                        }
-                                    </ul>)
-                                }
-                                <form action="">
-                                    <textarea 
-                                        name="descripcion"
-                                        value={ descripcion }
-                                        onChange={ handleChange }
-                                    />
-                                </form>
-                            </div>
-                        </>)
+                        ? <MedDetail 
+                                medicamento={ medicamento }
+                            />
                         : (<div>
                             Loading
                         </div>)
