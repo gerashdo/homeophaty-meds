bfacff8bfcba965f86640b8cf9d8b03eeb599894
gerashdo <gerardo.jimenezarguelles@gmail.com>
2022-11-12 15:19:58 -0600

    Crear modal dialog

---
+++ src/components/iterface/DialogModal.jsx
@@ -0,0 +1,56 @@
+
+import { useEffect } from 'react'
+import { useRef } from 'react'
+import './dialog-modal.css'
+
+export const DialogModal = ({ title, content, open, onClose, onAccept }) => {
+
+    const ref = useRef(null)
+
+    useEffect(() => {
+        if( open ){
+            ref.current?.showModal()
+        }else{
+            ref.current?.close()
+        }
+    }, [open])
+
+    const handleAccept = () => {
+        onAccept()
+        onClose()
+    }
+
+    return (
+        <dialog 
+            ref={ ref }
+            className='dialog-modal'
+        >
+            <header
+                className='title'
+            >
+                <h4>{ title }</h4>
+            </header>
+            <p
+                className='content'
+            >
+                { content }
+            </p>
+            <footer
+                className='footer'
+            >
+                <button
+                    onClick={ () => onClose() }
+                    className='simple'
+                >
+                    Cancelar
+                </button>
+                <button
+                    onClick={ handleAccept }
+                    className='primary'
+                >
+                    Aceptar
+                </button>
+            </footer>
+        </dialog>
+    )
+}
