a13eb0aef6287467272abde8b5c98b6c56ff2526
gerashdo <gerardo.jimenezarguelles@gmail.com>
2022-12-28 16:28:40 -0600

    Add tests for searchMeds helper

---
+++ tests/fixtures/meds.js
@@ -0,0 +1,29 @@
+import { medicineTypes } from "../../src/helpers/medicineTypes";
+
+
+export const abrotanum30 = {
+    name: 'Abrotanum',
+    ch: '30',
+    id: 1,
+    description: 'Es abrotanum 30',
+    medicines: [],
+    type: medicineTypes.Medicamento
+}
+
+export const calendula30 = {
+    name: 'Calendula',
+    ch: '30',
+    id: 2,
+    description: 'Es calendula 30',
+    medicines: [],
+    type: medicineTypes.Medicamento
+}
+
+export const calendula200 = {
+    name: 'Calendula',
+    ch: '200',
+    id: 3,
+    description: 'Es calendula 200',
+    medicines: [],
+    type: medicineTypes.Medicamento
+}
---
+++ tests/helpers/searchMeds.test.js
@@ -0,0 +1,23 @@
+import { deleteStringWhiteSpaces, searchStringInMed } from "../../src/helpers/searchMeds";
+import { abrotanum30, calendula200, calendula30 } from "../fixtures/meds";
+
+
+describe('tests for searchMeds helper', () => {
+    
+    test('should return a string with no blank spaces from the values of an array', () => {
+        const strings = [ ' i do', ' not ', 'want to have', 'spaces ' ]
+
+        const result = deleteStringWhiteSpaces( strings )
+        
+        expect( result ).toEqual( 'idonotwanttohavespaces' )
+    });
+
+    test('should return a list with the meds that contains the string', () => {
+        const medsArray = [ calendula200, calendula30, abrotanum30 ]
+
+        const result = searchStringInMed( 'cal', medsArray )
+
+        expect( result.length ).toBe( 2 )
+        
+    });
+});
