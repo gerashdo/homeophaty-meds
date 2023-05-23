a10a53481dc8b8aac7fb753b03e6c8fe2dfd253f
gerashdo <gerardo.jimenezarguelles@gmail.com>
2023-05-22 20:25:35 -0600

    Format files

--- src/hooks/useMedsStore.js
+++ src/hooks/useMedsStore.js
@@ -1,155 +1,145 @@
-import { useDispatch, useSelector } from "react-redux"
-import { 
-    addMedicamento, 
-    changeLoadingMedicamentos, 
-    deleteMedicamento, 
-    setMedicamentos, 
-    updateMedicamento 
-} from "../store/slices/medicamentos"
-import { setPageInformation, startAlert } from "../store/slices/ui"
-import { fetchAPI } from "./apiFetch"
-import { createContentErrorAlert } from "../helpers"
-
+import { useDispatch, useSelector } from 'react-redux'
+import {
+  addMedicamento,
+  changeLoadingMedicamentos,
+  deleteMedicamento,
+  setMedicamentos,
+  updateMedicamento
+} from '../store/slices/medicamentos'
+import { setPageInformation, startAlert } from '../store/slices/ui'
+import { fetchAPI } from './apiFetch'
+import { createContentErrorAlert } from '../helpers'
 
 export const useMedsStore = () => {
+  const dispatch = useDispatch()
+  const {
+    isLoading,
+    page,
+    medicamentos,
+    searchValue
+  } = useSelector(state => state.medicamento)
 
-    const dispatch = useDispatch()
-    const {
-        isLoading,
-        page,
-        medicamentos,
-        searchValue
-    } = useSelector( state => state.medicamento )
+  const { authToken } = useSelector(state => state.auth)
 
-    const { authToken } = useSelector( state => state.auth )
+  const startLoadingMedicamentos = async (queryParams) => {
+    try {
+      dispatch(changeLoadingMedicamentos())
 
-    const startLoadingMedicamentos = async ( queryParams ) => {
-        try {
+      const response = await fetchAPI({ endpoint: 'medicine', queryParams })
+      const data = await response.json()
 
-            dispatch( changeLoadingMedicamentos() )
-    
-            const response = await fetchAPI({ endpoint: 'medicine', queryParams })
-            const data = await response.json()
-                
-            if( response.status !== 200 ){
-                dispatch( startAlert( createContentErrorAlert( data ) ) )
-                dispatch( changeLoadingMedicamentos() )
-            }else{
-                dispatch( setMedicamentos({
-                    medicamentos: data.medicines
-                }))
-                dispatch( setPageInformation({
-                    page: Number(data.page),
-                    totalPages: data.totalPages,
-                }))
-            }
-        } catch (error) {
-            dispatch( changeLoadingMedicamentos() )
-            dispatch( startAlert({
-                alertMessage: 'Error interno, contacte al administrador',
-                alertType: 'error'
-            }) )
-        }
-    
+      if (response.status !== 200) {
+        dispatch(startAlert(createContentErrorAlert(data)))
+        dispatch(changeLoadingMedicamentos())
+      } else {
+        dispatch(setMedicamentos({
+          medicamentos: data.medicines
+        }))
+        dispatch(setPageInformation({
+          page: Number(data.page),
+          totalPages: data.totalPages
+        }))
+      }
+    } catch (error) {
+      dispatch(changeLoadingMedicamentos())
+      dispatch(startAlert({
+        alertMessage: 'Error interno, contacte al administrador',
+        alertType: 'error'
+      }))
     }
+  }
 
-    const startAddNewMedicamento = async ( medicamento ) => {
+  const startAddNewMedicamento = async (medicamento) => {
+    try {
+      const response = await fetchAPI({
+        endpoint: 'medicine',
+        data: medicamento,
+        method: 'POST',
+        token: authToken
+      })
 
-        try {
-            const response = await fetchAPI({
-                endpoint: 'medicine', 
-                data: medicamento, 
-                method: 'POST',
-                token: authToken
-            })
+      const data = await response.json()
 
-            const data = await response.json()
-    
-            if( response.status === 201 ){
-                dispatch( addMedicamento({ 
-                    medicamento: data.medicine 
-                }))
-                dispatch( startAlert({
-                    alertMessage: 'Nuevo medicamento guardado',
-                    alertType: 'success'
-                }))
-            }else{
-                dispatch( startAlert( createContentErrorAlert( data ) ) )
-            }
-            
-        } catch (error) {
-            dispatch( startAlert({
-                alertMessage: 'Error interno, contacte al administrador',
-                alertType: 'error'
-            }) )
-        }
+      if (response.status === 201) {
+        dispatch(addMedicamento({
+          medicamento: data.medicine
+        }))
+        dispatch(startAlert({
+          alertMessage: 'Nuevo medicamento guardado',
+          alertType: 'success'
+        }))
+      } else {
+        dispatch(startAlert(createContentErrorAlert(data)))
+      }
+    } catch (error) {
+      dispatch(startAlert({
+        alertMessage: 'Error interno, contacte al administrador',
+        alertType: 'error'
+      }))
     }
+  }
 
-    const startUpdateMedicamento = async ( medId, medData ) => {
-        try {
+  const startUpdateMedicamento = async (medId, medData) => {
+    try {
+      const response = await fetchAPI({
+        endpoint: `medicine/${medId}`,
+        data: medData,
+        method: 'PUT',
+        token: authToken
+      })
+      const data = await response.json()
 
-            const response = await fetchAPI({
-                endpoint: `medicine/${ medId }`,
-                data: medData,
-                method: 'PUT',
-                token: authToken
-            })
-            const data = await response.json()
-
-            if( response.status === 200 ){
-                dispatch( updateMedicamento( data.medicine ) )
-                dispatch( startAlert({
-                    alertMessage: 'El medicamento se ha modificado exitosamente',
-                    alertType: 'success'
-                }))
-            }else{
-                dispatch( startAlert( createContentErrorAlert( data ) ) )
-            }
-            
-        } catch (error) {
-            dispatch( startAlert({
-                alertMessage: 'Error interno, contacte al administrador',
-                alertType: 'error'
-            }) )
-        }
+      if (response.status === 200) {
+        dispatch(updateMedicamento(data.medicine))
+        dispatch(startAlert({
+          alertMessage: 'El medicamento se ha modificado exitosamente',
+          alertType: 'success'
+        }))
+      } else {
+        dispatch(startAlert(createContentErrorAlert(data)))
+      }
+    } catch (error) {
+      dispatch(startAlert({
+        alertMessage: 'Error interno, contacte al administrador',
+        alertType: 'error'
+      }))
     }
+  }
 
-    const startDeleteMedicamento = async ( medId ) => {
-        try {
+  const startDeleteMedicamento = async (medId) => {
+    try {
+      const response = await fetchAPI({
+        endpoint: `medicine/${medId}`,
+        method: 'DELETE',
+        token: authToken
+      })
+      const data = await response.json()
 
-            const response = await fetchAPI({
-                endpoint: `medicine/${medId}`,
-                method: 'DELETE',
-                token: authToken
-            })
-            const data = await response.json()
-            
-            if( response.status === 202 ){
-                dispatch( deleteMedicamento({ id: medId }) )
-                dispatch( startAlert({
-                    alertMessage: 'El medicamento ha sido eliminado',
-                    alertType: 'success'
-                }))
-            }else{
-                dispatch( startAlert( createContentErrorAlert( data ) ) )
-            }
-
-        } catch (error) {
-            dispatch( startAlert({
-                alertMessage: 'Error interno, contacte al administrador',
-                alertType: 'error',
-            }))
-        }
+      if (response.status === 202) {
+        dispatch(deleteMedicamento({ id: medId }))
+        dispatch(startAlert({
+          alertMessage: 'El medicamento ha sido eliminado',
+          alertType: 'success'
+        }))
+      } else {
+        dispatch(startAlert(createContentErrorAlert(data)))
+      }
+    } catch (error) {
+      dispatch(startAlert({
+        alertMessage: 'Error interno, contacte al administrador',
+        alertType: 'error'
+      }))
     }
+  }
 
-    return {
-        isLoading,
-        page,
-        medicamentos,
-        searchValue,
-        startLoadingMedicamentos,
-        startAddNewMedicamento,
-        startUpdateMedicamento,
-        startDeleteMedicamento,
-    }
+  return {
+    isLoading,
+    page,
+    medicamentos,
+    searchValue,
+    startLoadingMedicamentos,
+    startAddNewMedicamento,
+    startUpdateMedicamento,
+    startDeleteMedicamento
+  }
 }
--- src/store/slices/medicamentos/medicamentoSlice.js
+++ src/store/slices/medicamentos/medicamentoSlice.js
@@ -1,45 +1,45 @@
 import { createSlice } from '@reduxjs/toolkit'
 
 const initialState = {
-    isLoading: false,
-    medicamentos: [],
-    searchValue: ''
+  isLoading: false,
+  medicamentos: [],
+  searchValue: ''
 }
 
 export const medicamentoSlice = createSlice({
-    name: 'medicamento',
-    initialState,
-    reducers: {
-        changeLoadingMedicamentos: ( state ) => {
-            state.isLoading = !state.isLoading
-        },
-        setMedicamentos: (state, action ) => {
-            state.isLoading = false
-            state.page = action.payload.page
-            state.medicamentos = action.payload.medicamentos
-        },
-        addMedicamento: (state, action ) => {
-            state.medicamentos = [ ...state.medicamentos, action.payload.medicamento ]
-        },
-        updateMedicamento: ( state, action ) => {
-            state.medicamentos = state.medicamentos.map( med => (
-                med.id === action.payload.id ? action.payload : med
-            ))
-        },
-        deleteMedicamento: ( state, action ) => {
-            state.medicamentos = state.medicamentos.filter( med => med.id !== action.payload.id )
-        },
-        changeSearchValue: ( state, action ) => {
-            state.searchValue = action.payload
-        }
+  name: 'medicamento',
+  initialState,
+  reducers: {
+    changeLoadingMedicamentos: (state) => {
+      state.isLoading = !state.isLoading
     },
+    setMedicamentos: (state, action) => {
+      state.isLoading = false
+      state.page = action.payload.page
+      state.medicamentos = action.payload.medicamentos
+    },
+    addMedicamento: (state, action) => {
+      state.medicamentos = [...state.medicamentos, action.payload.medicamento]
+    },
+    updateMedicamento: (state, action) => {
+      state.medicamentos = state.medicamentos.map(med => (
+        med.id === action.payload.id ? action.payload : med
+      ))
+    },
+    deleteMedicamento: (state, action) => {
+      state.medicamentos = state.medicamentos.filter(med => med._id !== action.payload.id)
+    },
+    changeSearchValue: (state, action) => {
+      state.searchValue = action.payload
+    }
+  }
 })
 
-export const { 
-    changeLoadingMedicamentos, 
-    setMedicamentos, 
-    addMedicamento, 
-    updateMedicamento, 
-    deleteMedicamento,
-    changeSearchValue,
+export const {
+  changeLoadingMedicamentos,
+  setMedicamentos,
+  addMedicamento,
+  updateMedicamento,
+  deleteMedicamento,
+  changeSearchValue
 } = medicamentoSlice.actions
