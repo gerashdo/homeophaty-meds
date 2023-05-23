d83f8af37b7a941019c8d3a919f27359e3b85edc
gerashdo <gerardo.jimenezarguelles@gmail.com>
2023-05-22 20:25:26 -0600

    Change medicines list for a table

--- src/components/medicamentos/MedList.jsx
+++ src/components/medicamentos/MedList.jsx
@@ -1,14 +1,20 @@
-import { useEffect, useState } from 'react'
-import { searchStringInMed } from '../../helpers'
-import { MedCard } from './MedCard'
+import { useEffect, useRef, useState } from 'react'
+import { CgTrash, CgPen } from 'react-icons/cg'
+import { useNavigate } from 'react-router-dom'
+
 import { useMedsStore } from '../../hooks'
+import { DialogModal } from '../iterface/DialogModal'
+import { useModal } from '../../hooks/useModal'
+import { searchStringInMed } from '../../helpers'
 
 import './med-list.css'
 
 export const MedList = ({ searchVariable }) => {
   const { medicamentos, startDeleteMedicamento } = useMedsStore()
-
+  const { isOpen, closeModal, openModal } = useModal()
   const [medsList, setMedsList] = useState(medicamentos)
+  const medicineIdToDelete = useRef('')
+  const navigate = useNavigate()
 
   useEffect(() => {
     if (searchVariable) {
@@ -18,19 +24,69 @@ export const MedList = ({ searchVariable }) => {
     }
   }, [medicamentos, searchVariable])
 
-  const onCloseCard = (medId) => {
-    startDeleteMedicamento(medId)
+  const setMedicineToDelete = (medId) => {
+    medicineIdToDelete.current = medId
+    openModal()
+  }
+
+  const deleteMedicine = () => {
+    if (!medicineIdToDelete.current) return
+    startDeleteMedicamento(medicineIdToDelete.current)
+    medicineIdToDelete.current = ''
+  }
+
+  const navigateToMedicine = (medId) => {
+    navigate(`/medicamentos/${medId}`)
   }
 
   return (
-    <div id='med-list' className='med-list'>
-      {medsList.map(med => (
-        <MedCard
-          key={med._id}
-          medicamento={med}
-          onCloseCard={onCloseCard}
-        />
-      ))}
-    </div>
+    <>
+      <table className='med-list' cellSpacing='0'>
+        <thead>
+          <tr>
+            <th>Nombre</th>
+            <th>CH</th>
+            <th>Tipo</th>
+            <th />
+          </tr>
+        </thead>
+        <tbody>
+          {
+            medsList.map(med => (
+              <tr key={med._id}>
+                <td
+                  className='name'
+                  onClick={() => navigateToMedicine(med._id)}
+                >
+                  {med.name}
+                </td>
+                <td>{med.ch}</td>
+                <td>{med.type}</td>
+                <td className='actions'>
+                  <button
+                    className='action'
+                    onClick={() => setMedicineToDelete(med._id)}
+                  >
+                    <CgTrash />
+                  </button>
+                  <button
+                    className='action'
+                  >
+                    <CgPen />
+                  </button>
+                </td>
+              </tr>
+            ))
+          }
+        </tbody>
+      </table>
+      <DialogModal
+        title='Confirmación de Eliminación'
+        content='¿Estas seguro de que quieres eliminar este medicamento?'
+        open={isOpen}
+        onClose={closeModal}
+        onAccept={deleteMedicine}
+      />
+    </>
   )
 }
