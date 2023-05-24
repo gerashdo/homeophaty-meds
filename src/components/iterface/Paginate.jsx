fba1f36d75f600fb206f1d260e2b3b3c792cb14e
gerashdo <gerardo.jimenezarguelles@gmail.com>
2023-05-23 18:19:10 -0600

    Change paginate style

--- src/components/iterface/Paginate.jsx
+++ src/components/iterface/Paginate.jsx
@@ -1,37 +1,33 @@
-import { PaginateItem } from "./PaginateItem"
-import { usePaginate } from "../../hooks/usePaginate"
+import { PaginateItem } from './PaginateItem'
+import { usePaginate } from '../../hooks/usePaginate'
 import PropTypes from 'prop-types'
 
 import './paginate.css'
 
-
 export const Paginate = ({ onClickItem, totalPages, currentPage: actualPage }) => {
+  const { setCurrentPage, currentPage, pages } = usePaginate(totalPages, actualPage)
 
-    const { setCurrentPage, currentPage, pages } = usePaginate( totalPages, actualPage )
+  const onItemClick = (number) => {
+    setCurrentPage(number)
+    onClickItem(number)
+  }
 
-    const onItemClick = ( number ) => {
-        setCurrentPage( number )
-        onClickItem( number )
-    }
-
-    return (
-        <div className="paginate-container">
-            {
-                pages.map( page => (
-                    <PaginateItem 
-                        key={ page }
-                        number={ page }
-                        active={ currentPage === page ? true : false }
-                        onItemClick={ onItemClick }
-                    />
-                ))
-            }
-        </div>
-    )
+  return (
+    <div className='paginate-container'>
+      {pages.map(page => (
+        <PaginateItem
+          key={page}
+          number={page}
+          active={currentPage === page}
+          onItemClick={onItemClick}
+        />
+      ))}
+    </div>
+  )
 }
 
 Paginate.propTypes = {
-    onClickItem: PropTypes.func.isRequired,
-    totalPages: PropTypes.number.isRequired,
-    currentPage: PropTypes.number.isRequired,
+  onClickItem: PropTypes.func.isRequired,
+  totalPages: PropTypes.number.isRequired,
+  currentPage: PropTypes.number.isRequired
 }
--- src/components/iterface/PaginateItem.jsx
+++ src/components/iterface/PaginateItem.jsx
@@ -4,24 +4,23 @@ import PropTypes from 'prop-types'
 import './paginate-item.css'
 
 export const PaginateItem = ({ number, onItemClick, active = false }) => {
+  const onClick = () => {
+    onItemClick(number)
+  }
 
-    const onClick = () => {
-        onItemClick( number )
-    }
-
-    return (
-        <div
-            className={`paginate-item ${ active ? 'active' : '' }` }
-            onClick={ onClick }
-            data-testid="item-container"
-        >
-            { number }
-        </div>
-    )
+  return (
+    <div
+      className={`paginate-item ${active ? 'active' : ''}`}
+      onClick={onClick}
+      data-testid='item-container'
+    >
+      {number}
+    </div>
+  )
 }
 
 PaginateItem.propTypes = {
-    number: PropTypes.number.isRequired,
-    onItemClick: PropTypes.func.isRequired,
-    active: PropTypes.bool,
+  number: PropTypes.number.isRequired,
+  onItemClick: PropTypes.func.isRequired,
+  active: PropTypes.bool
 }
