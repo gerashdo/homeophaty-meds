da2e9fe2caea24237286c02ea6eacb6787fd4d75
gerashdo <gerardo.jimenezarguelles@gmail.com>
2024-02-29 13:55:17 -0600

    Include prescriptions to medicine detail

--- src/components/medicamentos/MedDetail.jsx
+++ src/components/medicamentos/MedDetail.jsx
@@ -1,42 +1,35 @@
-import { MedSmallCard } from "./MedSmallCard"
-import { medicineTypes } from "../../helpers"
+import { MedSmallCard } from './MedSmallCard'
+import { medicineTypes } from '../../helpers'
 
 import './med-detail.css'
 
 export const MedDetail = ({ medicamento }) => {
-    
-    return (
-        <>
-            <div className="med-titlename">
-                <h3>{ medicamento.name } { medicamento.ch }</h3>
-            </div>
-            <div className="med-data">
-                {
-                    medicamento.type === medicineTypes.Medicamento
-                    ? null 
-                    :(<div className="inner-med-list">
-                        <h4>Medicamentos</h4>
-                        {
-                            medicamento.medicines.map( med => (
-                                <MedSmallCard
-                                    key={ med._id || med.id } 
-                                    medicamento={ med }
-                                    isDelete={ false }
-                                />
-                            ))
-                        }
-                    </div>)
-                }
-                <div className="med-description">
-                    <h4>Prescripcion</h4>
-                    <textarea 
-                        name="descripcion"
-                        value={ medicamento.description }
-                        rows='20'
-                        readOnly
-                    />
-                </div>
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div className='med-titlename'>
+        <h3>{medicamento.name} {medicamento.ch}</h3>
+      </div>
+      <div className='med-data'>
+        {medicamento.type === medicineTypes.Medicamento
+          ? null
+          : (
+            <div className='inner-med-list'>
+              <h4>Medicamentos</h4>
+              {medicamento.medicines.map(med => (
+                <MedSmallCard
+                  key={med._id}
+                  medicamento={med}
+                  isDelete={false}
+                />
+              ))}
+            </div>)}
+        <div className='med-description'>
+          <h4>Prescripciones</h4>
+          {medicamento.prescription.map(pres => (
+            <p key={pres._id}>{pres.description}</p>
+          ))}
+        </div>
+      </div>
+    </>
+  )
 }
--- src/components/medicamentos/MedDetailEdit.jsx
+++ src/components/medicamentos/MedDetailEdit.jsx
@@ -1,135 +1,143 @@
-import { useDispatch } from "react-redux"
-import { useForm } from "../../hooks/useForm"
-import { chOptions, medExists, medicineTypes } from "../../helpers"
-import { RadioOptions } from "../iterface/RadioOptions"
-import { startAlert } from "../../store/slices/ui"
-import { InnerMedsForm } from "./InnerMedsForm"
-import { useMedsStore } from "../../hooks"
+import { useDispatch } from 'react-redux'
+import { useForm } from '../../hooks/useForm'
+import { chOptions, medExists, medicineTypes } from '../../helpers'
+import { RadioOptions } from '../iterface/RadioOptions'
+import { startAlert } from '../../store/slices/ui'
+import { InnerMedsForm } from './InnerMedsForm'
+import { useMedsStore } from '../../hooks'
 
 import './med-detail-edit.css'
 
 export const MedDetailEdit = ({ medicamento, onCancel }) => {
-    const dispatch = useDispatch()
-    const { startUpdateMedicamento, medicamentos } = useMedsStore()
+  const dispatch = useDispatch()
+  const { startUpdateMedicamento, medicamentos } = useMedsStore()
 
-    const [ values, handleChange, setValues ] = useForm({
-        name: medicamento.name,
-        description: medicamento.description,
-        ch: medicamento.ch
-     })
-    const { description, name, ch } = values
+  const [values, handleChange, setValues] = useForm({
+    name: medicamento.name,
+    description: medicamento.description,
+    ch: medicamento.ch
+  })
+  const { description, name, ch } = values
 
-    // TODO: Agregar dispatch para guardar la descripcion del medicamento
-    const handleSubmit = (e) => {
-        e.preventDefault()
+  // TODO: Agregar dispatch para guardar la descripcion del medicamento
+  const handleSubmit = (e) => {
+    e.preventDefault()
 
-        const medData = { 
-            ...medicamento,
-            description,
-            ch,
-            name
-        }
-
-        if( medExists( medicamentos, medicamento._id , medData ) ){
-            return dispatch( startAlert({
-                alertMessage: 'El medicamento ya existe',
-                alertType: 'error',
-            }))
-        }
-
-        // TODO: Utilizar un estas seguro por si viene vacio
-        startUpdateMedicamento( medicamento._id, medData )
-        onCancel()
+    const medData = {
+      ...medicamento,
+      description,
+      ch,
+      name
     }
 
-    const handleRemoveMed = ( id ) => {
-        const medUpdated = { 
-            ...medicamento, 
-            medicines: medicamento.medicines.filter( med => med._id !== id )
-        }
-
-        startUpdateMedicamento( medicamento._id, medUpdated )
+    if (medExists(medicamentos, medicamento._id, medData)) {
+      return dispatch(startAlert({
+        alertMessage: 'El medicamento ya existe',
+        alertType: 'error'
+      }))
     }
 
-    const handleOnChangeInnerMeds = ( newInnerMed ) => {
-        const newInnerMeds = [
-            ...medicamento.medicines,
-            { ...newInnerMed, '_id': newInnerMed.id }
-        ]
-        const medUpdated = {
-            ...medicamento,
-            medicines: newInnerMeds
-        }
-        startUpdateMedicamento( medicamento._id, medUpdated )
+    // TODO: Utilizar un estas seguro por si viene vacio
+    startUpdateMedicamento(medicamento._id, medData)
+    onCancel()
+  }
+
+  const handleRemoveMed = (id) => {
+    const medUpdated = {
+      ...medicamento,
+      medicines: medicamento.medicines.filter(med => med._id !== id)
     }
 
-    const handleChChange = ( e ) => {
-        setValues({ ...values, ch: e.target.value })
+    startUpdateMedicamento(medicamento._id, medUpdated)
+  }
+
+  const handleOnChangeInnerMeds = (newInnerMed) => {
+    const newInnerMeds = [
+      ...medicamento.medicines,
+      { ...newInnerMed, _id: newInnerMed.id }
+    ]
+    const medUpdated = {
+      ...medicamento,
+      medicines: newInnerMeds
     }
-    
-    return (
-        <>
-            <div className="med-titlename">
-                <input
-                    name="name"
-                    type="text"
-                    value={ name }
-                    onChange={ handleChange }
-                />
-            </div>
-            {
-                medicamento.type === medicineTypes.Medicamento
-                ? (
-                    <div className="med-ch">
-                        <RadioOptions 
-                            options={ chOptions }
-                            value={ ch }
-                            onChange={ handleChChange }
-                        />
-                    </div>
-                )
-                : null
-            }
-            <div className="med-data">
-                {/* TODO: No mostrar el formulario si tiene un ch el medicamento  */}
-                {
-                    medicamento.type === medicineTypes.Medicamento
-                    ? null 
-                    :(
-                        <div className="inner-med-list">
-                            <h4>Medicamentos</h4>
-                            <InnerMedsForm
-                                innerMeds={ medicamento.medicines }
-                                onRemoveInnerMeds={ handleRemoveMed }
-                                onChangeInnerMeds={ handleOnChangeInnerMeds }
-                            />
-                        </div>
-                    )
-                }
-                <form
-                    onSubmit={ handleSubmit }
-                >
-                    <h4>Prescripcion</h4>
-                    <textarea 
-                        name="description"
-                        value={ description }
-                        onChange={ handleChange }
-                        rows='20'
-                    />
-                    <div className="align-content-end submit-button-container">
-                        <button 
-                            type="submit"
-                            className="simple"
-                            onClick={ onCancel }
-                        >Cancelar</button>
-                        <button 
-                            type="submit"
-                            className="primary"
-                        >Guardar cambios</button>
-                    </div>
-                </form>
-            </div>
-        </>
-    )
+    startUpdateMedicamento(medicamento._id, medUpdated)
+  }
+
+  const handleChChange = (e) => {
+    setValues({ ...values, ch: e.target.value })
+  }
+
+  return (
+    <>
+      <div className='med-titlename'>
+        <input
+          name='name'
+          type='text'
+          value={name}
+          onChange={handleChange}
+        />
+      </div>
+      {medicamento.type === medicineTypes.Medicamento
+        ? (
+          <div className='med-ch'>
+            <RadioOptions
+              options={chOptions}
+              value={ch}
+              onChange={handleChChange}
+            />
+          </div>
+          )
+        : null}
+      <div className='med-data'>
+        {/* TODO: No mostrar el formulario si tiene un ch el medicamento  */}
+        {medicamento.type === medicineTypes.Medicamento
+          ? null
+          : (
+            <div className='inner-med-list'>
+              <h4>Medicamentos</h4>
+              <InnerMedsForm
+                innerMeds={medicamento.medicines}
+                onRemoveInnerMeds={handleRemoveMed}
+                onChangeInnerMeds={handleOnChangeInnerMeds}
+              />
+            </div>)}
+        <div className='med-description'>
+          <div>
+            <h4>Prescripciones</h4>
+            <button>
+              Agregar prescripcion
+            </button>
+          </div>
+          {medicamento.prescription.map(pres => (
+            <p key={pres._id}>{pres.description}</p>
+          ))}
+        
+        <form
+          onSubmit={handleSubmit}
+        >
+          <h4>Prescripcion</h4>
+          <textarea
+            name='description'
+            value={description}
+            onChange={handleChange}
+            rows='20'
+          />
+          <div className='align-content-end submit-button-container'>
+            <button
+              type='submit'
+              className='simple'
+              onClick={onCancel}
+            >Cancelar
+            </button>
+            <button
+              type='submit'
+              className='primary'
+            >Guardar cambios
+            </button>
+          </div>
+        </form>
+        </div>
+      </div>
+    </>
+  )
 }
-
